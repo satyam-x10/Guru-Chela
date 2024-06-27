26e8d6e160c8a55fbbab277a0738bbc9f2c5a28e
SATYAM KUMAR <satyamx10@gmail.com>
2024-06-27 15:08:57 +0530

    stable

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,14 @@ import { connectDB } from "./config/Database.js";
 connectDB();
 
 const app = express();
-
+// Using Middlewares
+app.use(express.json({ limit: '50mb' }));
+app.use(
+  express.urlencoded({
+    limit: '50mb',
+    extended: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(
